aa856fff3cb7b2a1e437743b5b0e2d69880d10b6
Matt Thompson <matthew.thompson.a@gmail.com>
2017-06-18 14:37:54 -0400

    Docs and hash_input

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,51 @@
 defmodule Identicon do
   @moduledoc """
-  Documentation for Identicon.
+  Methods for creating an Identicon based on hashed input
+  Input retuns a list of 16.
+  Identicons are mirrored down the center.
+  Using the first 15 values to determine a 5x5 square.
+  [1,2,3,2,1]
+  [4,5,6,5,4]
+  [7,8,9,8,7]
+  [10,11,12,11,10]
+  [13,14,15,14,15]
+  If value is even color the square...
+  First three values also determine RGB color. 
   """
 
   @doc """
-  Hello world.
+    Main ...
+  """
+  def main(input) do
+    input
+    |> hash_input
+  end
+
+  @doc """
+    `hash_input` takes an input and returns a md5 binary hash
 
   ## Examples
 
-      iex> Identicon.hello
-      :world
+      iex> Identicon.hash_input("banana")
+      [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]
 
   """
-  def hello do
-    :world
+  def hash_input(input) do
+    :crypto.hash(:md5, input)
+    |>:binary.bin_to_list
   end
+
+  @doc """
+    `dd`
+
+  ## Examples
+
+      iex> Identicon.hash_input("banana")
+      [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]
+
+  """
+  def  do
+
+  end
+
 end
