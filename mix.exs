51cd5223b0cd4118ebc41751bda35695249dea9c
Matt Thompson <matthew.thompson.a@gmail.com>
2017-06-18 18:42:03 -0400

    had to import module for egd

--- mix.exs
+++ mix.exs
@@ -29,7 +29,8 @@ defmodule Identicon.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:ex_doc, "~>0.12"}
+      {:ex_doc, "~>0.12"},
+      {:percept, github: 'erlang/percept'}
     ]
   end
 end
